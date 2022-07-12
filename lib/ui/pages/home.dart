d2459834344f2451e8abc3f04d9d408c0828e811
bluejoyq <bluejoy32@gmail.com>
2022-07-12 19:42:41 +0900

    feat: bottom navigation bar 약간 구현
    - bottom navigation bar 약간 구현

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -2,21 +2,42 @@ import 'package:climb_balance/providers/token.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class Home extends ConsumerWidget {
+class Home extends ConsumerStatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends ConsumerState {
+  int currentIdx = 0;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          child: Text('logout'),
+          child: const Text('logout'),
           onPressed: () {
             ref.read(tokenProvider.notifier).clearToken();
             Navigator.popAndPushNamed(context, '/auth');
           },
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIdx,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+          BottomNavigationBarItem(icon: Icon(Icons.add_a_photo), label: 'add'),
+        ],
+      ),
     );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      currentIdx = index;
+    });
   }
 }
