b2d9a9bef9c65022ece0c1b9741cc47ec824a86d
bluejoyq <bluejoy32@gmail.com>
2022-07-14 17:12:00 +0900

    feat(CB-99): video player 재생 확인.
    - video player 재생 확인

--- lib/ui/pages/home/home.dart
+++ lib/ui/pages/home/home.dart
@@ -1,8 +1,6 @@
 import 'package:climb_balance/providers/token.dart';
-import 'package:climb_balance/ui/widgets/botNavigationBar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:image_picker/image_picker.dart';
 
 class Home extends ConsumerStatefulWidget {
   const Home({Key? key}) : super(key: key);
--- lib/ui/pages/home/upload.dart
+++ lib/ui/pages/home/upload.dart
@@ -1,8 +1,81 @@
-import 'package:climb_balance/providers/token.dart';
-import 'package:climb_balance/ui/widgets/botNavigationBar.dart';
+import 'dart:io';
+
+import 'package:video_player/video_player.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
+
+class VideoPopup extends StatefulWidget {
+  XFile file;
+
+  VideoPopup({Key? key, required this.file}) : super(key: key);
+
+  @override
+  State<VideoPopup> createState() => _VideoPopupState();
+}
+
+class _VideoPopupState extends State<VideoPopup> {
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.file(File(widget.file.path))
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+    _initializeVideoPlayerFuture = _controller.initialize();
+    _controller.setLooping(true);
+  }
+
+  @override
+  dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          FutureBuilder(
+            future: _initializeVideoPlayerFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                // If the VideoPlayerController has finished initialization, use
+                // the data it provides to limit the aspect ratio of the video.
+                return AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  // Use the VideoPlayer widget to display the video.
+                  child: VideoPlayer(_controller),
+                );
+              } else {
+                // If the VideoPlayerController is still initializing, show a
+                // loading spinner.
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _controller.value.isPlaying
+                    ? _controller.pause()
+                    : _controller.play();
+              });
+            },
+            child: _controller.value.isPlaying ? Text('멈춤') : Text("재생"),
+          )
+        ],
+      ),
+    );
+  }
+}
 
 class Upload extends ConsumerStatefulWidget {
   const Upload({Key? key}) : super(key: key);
@@ -12,6 +85,27 @@ class Upload extends ConsumerStatefulWidget {
 }
 
 class UploadState extends ConsumerState {
+  final ImagePicker _picker = ImagePicker();
+
+  void _getVideoFromGallery(context) async {
+    final XFile? image = await _picker.pickVideo(source: ImageSource.gallery);
+    debugPrint(image?.path);
+    if (image == null) {
+      return;
+    }
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => VideoPopup(file: image)));
+  }
+
+  void _getVideoFromCamera() async {
+    final XFile? video = await _picker.pickVideo(source: ImageSource.camera);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,12 +116,16 @@ class UploadState extends ConsumerState {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ElevatedButton(
-                child: const Text('logout'),
+                child: const Text('갤러리에서 사진 가져오기'),
                 onPressed: () {
-                  ref.read(tokenProvider.notifier).clearToken();
-                  Navigator.popAndPushNamed(context, '/auth');
+                  _getVideoFromGallery(context);
                 },
               ),
+              ElevatedButton(
+                  onPressed: () {
+                    _getVideoFromCamera();
+                  },
+                  child: const Text('사진 촬영하기'))
             ],
           ),
         ),
