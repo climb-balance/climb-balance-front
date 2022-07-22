9b0e632beaba15955307ca5b8aeb9414c5e2feb3
bluejoyq <bluejoy32@gmail.com>
2022-07-23 01:49:50 +0900

    feat(CB-111): upload에서 수정된 모델 사용하게.
    - difficulty, location 모델 사용으로.

--- lib/providers/upload.dart
+++ lib/providers/upload.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
 
+import 'package:climb_balance/models/tag.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UploadType {
   double start, end;
-  int location, difficulty;
+  Location location;
+  Difficulty difficulty;
   bool success;
   DateTime date;
   String detail;
@@ -13,8 +15,8 @@ class UploadType {
   UploadType({
     this.start = -1,
     this.end = -1,
-    this.location = -1,
-    this.difficulty = -1,
+    required this.location,
+    required this.difficulty,
     this.success = false,
     this.detail = '',
     required this.date,
@@ -25,15 +27,19 @@ class UploadNotifier extends StateNotifier<UploadType> {
   final ref;
 
   // TODO state 불변으로 만들어야함!!!!!
-  UploadNotifier({required this.ref}) : super(UploadType(date: DateTime.now()));
+  UploadNotifier({required this.ref})
+      : super(UploadType(
+            location: Location(),
+            difficulty: Difficulty(),
+            date: DateTime.now()));
 
   void setFile({required File file}) {
     state.file = file;
   }
 
   void setTags(
-      {required int location,
-      required int difficulty,
+      {required Location location,
+      required Difficulty difficulty,
       required bool success,
       required DateTime date}) {
     state.location = location;
