d7a91ee6c30ebc3dbe5e3a8ee4e0bfc0c2884b2d
bluejoyq <bluejoy32@gmail.com>
2022-07-27 15:03:27 +0900

    feat: 임의 유저 생성 기능 추가
    - 테스트를 위해.

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -8,3 +8,11 @@ class UserProfile {
       required this.profileImagePath,
       required this.uniqueCode});
 }
+
+UserProfile genRandomUser() {
+  return UserProfile(
+    nickName: '1234',
+    profileImagePath: 'https://i.imgur.com/7yteESU.jpeg',
+    uniqueCode: 1234,
+  );
+}
