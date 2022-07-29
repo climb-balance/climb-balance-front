b1c7d0d47e6a9f6eed17fb86679ad26e56dc197b
bluejoyq <bluejoy32@gmail.com>
2022-07-30 00:49:25 +0900

    feat(CB-126): story filter 기능 추가.
    - aionly, expert only로 필터링 추가.

--- lib/ui/pages/diary/diary.dart
+++ lib/ui/pages/diary/diary.dart
@@ -1,8 +1,6 @@
-import 'dart:convert';
-
 import 'package:climb_balance/models/story.dart';
 import 'package:climb_balance/models/user.dart';
-import 'package:climb_balance/providers/api.dart';
+
 import 'package:climb_balance/providers/serverRequest.dart';
 import 'package:climb_balance/ui/widgets/botNavigationBar.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +9,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../configs/serverConfig.dart';
 import '../../widgets/profileInfo.dart';
 import '../../widgets/story.dart';
+
+enum FilterType { noFilter, aiOnly, expertOnly }
 
 class Diary extends ConsumerStatefulWidget {
   const Diary({Key? key}) : super(key: key);
@@ -22,7 +22,9 @@ class Diary extends ConsumerStatefulWidget {
 class _DiaryState extends ConsumerState<Diary> with TickerProviderStateMixin {
   late final UserProfile profile;
   late TabController _tabController;
-  late Map<String, List<Story>> classifiedStories = {};
+  late List<Story> stories;
+  late Map<String, List<Story>> treatedStories = {};
+  FilterType currentFilter = FilterType.noFilter;
 
   @override
   void initState() {
@@ -45,32 +47,47 @@ class _DiaryState extends ConsumerState<Diary> with TickerProviderStateMixin {
 
   void loadStories() async {
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
-      classifiedStories = <String, List<Story>>{};
+      List<Story> newStories = [];
       final body = await ref
           .read(serverRequestPrivider)
           .get(ServerUrl + ServerStoryPath);
       for (final data in body['stories']) {
         final story = Story.fromJson(data);
-        final String key = story.makeKey();
-        if (classifiedStories.containsKey(key)) {
-          classifiedStories[key]!.add(story);
-        } else {
-          classifiedStories[key] = [story];
-        }
+        newStories.add(story);
       }
-      setState(() {});
+      stories = newStories;
+      updateFilter(FilterType.noFilter);
     });
+  }
+
+  void updateFilter(FilterType filter) {
+    treatedStories = <String, List<Story>>{};
+    for (final story in stories) {
+      final String key = story.makeKey();
+      if (filter == FilterType.aiOnly && story.aiAvailable != 2) {
+        continue;
+      } else if (filter == FilterType.expertOnly &&
+          story.expertAvailable != 2) {
+        continue;
+      }
+      if (treatedStories.containsKey(key)) {
+        treatedStories[key]?.add(story);
+      } else {
+        treatedStories[key] = [story];
+      }
+    }
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
     return Scaffold(
       body: SafeArea(
         child: ListView(
           children: [
             ProfileInfo(profile: profile),
-            ...classifiedStories.values
+            FilterDropdown(updateFilter: updateFilter),
+            ...treatedStories.values
                 .map((stories) => ClassifiedStory(stories: stories))
                 .toList()
           ],
@@ -119,6 +136,45 @@ class DelegateTabBar extends SliverPersistentHeaderDelegate {
   @override
   bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
     return true;
+  }
+}
+
+class FilterDropdown extends StatefulWidget {
+  final void Function(FilterType) updateFilter;
+
+  const FilterDropdown({Key? key, required this.updateFilter})
+      : super(key: key);
+
+  @override
+  State<FilterDropdown> createState() => _FilterDropdownState();
+}
+
+class _FilterDropdownState extends State<FilterDropdown> {
+  FilterType currentDropdown = FilterType.noFilter;
+  static const Map<FilterType, String> _filters = {
+    FilterType.noFilter: "필터 없음",
+    FilterType.aiOnly: "ai 영상만",
+    FilterType.expertOnly: "전문가 영상만",
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<FilterType>(
+      value: currentDropdown,
+      items: _filters.keys
+          .map(
+            (key) => DropdownMenuItem<FilterType>(
+              value: key,
+              child: Text(_filters[key]!),
+            ),
+          )
+          .toList(),
+      onChanged: (newValue) {
+        currentDropdown = newValue!;
+        widget.updateFilter(newValue);
+        setState(() {});
+      },
+    );
   }
 }
 
