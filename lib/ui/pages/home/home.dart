723b995e2bed9266b579a0f39d7f0df60cb24505
bluejoyq <bluejoy32@gmail.com>
2022-07-31 06:13:21 +0900

    feat: 메인 페이지 통계 구현
    - 깃허브와 같은 heatmap과 연속일

--- lib/ui/pages/home/home.dart
+++ lib/ui/pages/home/home.dart
@@ -1,4 +1,6 @@
+import 'dart:math';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:climb_balance/ui/pages/auth/register.dart';
 import 'package:climb_balance/ui/widgets/safearea.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,8 +14,18 @@ class Home extends ConsumerStatefulWidget {
 }
 
 class HomeState extends ConsumerState {
+  List<int> loadDatas() {
+    Random random = Random();
+    List<int> result = [];
+    for (int i = 0; i < 30; i++) {
+      result.add(random.nextInt(10));
+    }
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Row(
@@ -23,10 +35,106 @@ class HomeState extends ConsumerState {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
-          children: [ImageBanner()],
+          children: [
+            Card(child: ImageBanner()),
+            SizedBox(
+              height: MediaQuery.of(context).size.width / 2 - 30,
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 2,
+                    child: HeatMap(
+                      datas: loadDatas(),
+                    ),
+                  ),
+                  Expanded(
+                    child: Card(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text('현재까지'),
+                          Text(
+                            '50일',
+                            style: theme.textTheme.headline2,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
       bottomNavigationBar: const BotNavigationBar(currentIdx: 0),
+    );
+  }
+}
+
+class HeatMap extends StatelessWidget {
+  final List<int> datas;
+
+  const HeatMap({Key? key, required this.datas}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: GridView.count(
+        padding: const EdgeInsets.all(5),
+        physics: const NeverScrollableScrollPhysics(),
+        mainAxisSpacing: 5,
+        crossAxisSpacing: 5,
+        crossAxisCount: 6,
+        children: datas.map((data) => HeatMapSquare(data: data)).toList(),
+      ),
+    );
+  }
+}
+
+class HeatMapSquare extends StatefulWidget {
+  final int data;
+  static const maxValue = 11;
+
+  const HeatMapSquare({Key? key, required this.data}) : super(key: key);
+
+  @override
+  State<HeatMapSquare> createState() => _HeatMapSquareState();
+}
+
+class _HeatMapSquareState extends State<HeatMapSquare> {
+  bool _show = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _show = true;
+        });
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: theme.colorScheme.primary.withOpacity(
+            min(1 - (widget.data / HeatMapSquare.maxValue), 1),
+          ),
+        ),
+        child: AnimatedOpacity(
+            opacity: _show ? 1.0 : 0.0,
+            duration: const Duration(milliseconds: 1000),
+            child: Center(
+              child: Text(
+                widget.data.toString(),
+              ),
+            ),
+            onEnd: () {
+              setState(() {
+                _show = false;
+              });
+            }),
+      ),
     );
   }
 }
@@ -42,14 +150,15 @@ class _ImageBannerState extends State<ImageBanner> {
   static const double _height = 200;
   final List<String> images = [
     'https://www.navercorp.com/navercorp_/promotion/tvAds/2021/20210803115600_1.png',
-    'https://www.navercorp.com/navercorp_/promotion/tvAds/2021/20210803115600_1.png',
-    'https://www.navercorp.com/navercorp_/promotion/tvAds/2021/20210803115600_1.png'
+    'https://img.freepik.com/free-vector/abstract-website-banner-with-modern-shapes_1361-1738.jpg?w=2000',
+    'https://img.freepik.com/free-vector/modern-website-banner-template-with-abstract-shapes_1361-3311.jpg?w=2000'
   ];
   int current = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    final theme = Theme.of(context);
+    return Stack(
       children: [
         CarouselSlider(
           items: images
@@ -66,6 +175,7 @@ class _ImageBannerState extends State<ImageBanner> {
               .toList(),
           options: CarouselOptions(
             height: _height,
+            autoPlay: true,
             viewportFraction: 1,
             onPageChanged: (index, _) {
               setState(() {
@@ -74,25 +184,33 @@ class _ImageBannerState extends State<ImageBanner> {
             },
           ),
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: images.map(
-            (image) {
-              int index = images.indexOf(image);
-              return Container(
-                width: 8.0,
-                height: 8.0,
-                margin:
-                    const EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: current == index
-                      ? Color.fromRGBO(0, 0, 0, 0.9)
-                      : Color.fromRGBO(0, 0, 0, 0.4),
-                ),
-              );
-            },
-          ).toList(),
+        Positioned.fill(
+          child: Align(
+            alignment: AlignmentDirectional.bottomCenter,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: images.map(
+                (image) {
+                  int index = images.indexOf(image);
+                  return Container(
+                    width: 10,
+                    height: 10,
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 2.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: theme.colorScheme.surface,
+                      ),
+                      shape: BoxShape.circle,
+                      color: current == index
+                          ? theme.colorScheme.primary
+                          : theme.colorScheme.onSurface,
+                    ),
+                  );
+                },
+              ).toList(),
+            ),
+          ),
         ),
       ],
     );
