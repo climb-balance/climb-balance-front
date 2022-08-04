be622b4a967dd34ffcd92d03aefa7dbf6df592b6
bluejoyq <bluejoy32@gmail.com>
2022-08-05 02:52:51 +0900

    feat(CB-147): 갱신된 새로운 텍스트 테마 이용하도록 반영
    - 갱신된 새로운 텍스트 테마 이용하도록 반영

--- lib/ui/theme/main_theme.dart
+++ lib/ui/theme/main_theme.dart
@@ -1,10 +1,12 @@
 import 'package:climb_balance/ui/theme/color_schema.dart';
+import 'package:climb_balance/ui/theme/text_theme.dart';
 import 'package:flutter/material.dart';
 
 ThemeData mainLightTheme() {
   return ThemeData().copyWith(
     colorScheme: lightColorScheme,
     extensions: <ThemeExtension<dynamic>>[],
+    textTheme: customTextTheme(ThemeData.light().textTheme, lightColorScheme),
   );
 }
 
@@ -12,5 +14,6 @@ ThemeData mainDarkTheme() {
   return ThemeData.dark().copyWith(
     colorScheme: darkColorScheme,
     extensions: <ThemeExtension<dynamic>>[],
+    textTheme: customTextTheme(ThemeData.dark().textTheme, darkColorScheme),
   );
 }
