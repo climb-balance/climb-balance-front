ce32e5d34e4d741373e5599743216b00ff83df3c
bluejoyq <bluejoy32@gmail.com>
2022-08-07 00:33:15 +0900

    feat: userProfile model 변경
    - userProfile model에서 token과 isExpert도 관리하도록.

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,6 +2,8 @@ class UserProfile {
   String nickName;
   String profileImagePath;
   String introduce;
+  String token;
+  bool isExpert;
   int uniqueCode;
   int height;
   int weight;
@@ -13,10 +15,24 @@ class UserProfile {
     this.introduce = '',
     this.height = -1,
     this.weight = -1,
+    this.token = '',
+    this.isExpert = false,
   });
 }
 
-UserProfile genRandomUser() {
+UserProfile genRandomUser({bool isExpert = false}) {
+  if (isExpert) {
+    return UserProfile(
+      nickName: '심규진',
+      profileImagePath:
+          'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/00/00480565a56a710bc697f41f9a31e617d1d7a989_full.jpg',
+      uniqueCode: 1234,
+      height: 160,
+      weight: 50,
+      introduce: '안녕하세요. 즐거운 클라이밍해요!!',
+      isExpert: true,
+    );
+  }
   return UserProfile(
     nickName: '심규진',
     profileImagePath:
