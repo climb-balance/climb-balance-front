6f69ac413c5e58a4f8e35fce2e7600f6345eaddf
bluejoyq <bluejoy32@gmail.com>
2022-08-18 10:25:26 +0900

    fix: 이미지 링크 업데이트

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -12,7 +12,7 @@ part 'user.g.dart';
 class UserProfile with _$UserProfile {
   const factory UserProfile({
     @Default('default') String nickName,
-    @Default('https://as1.ftcdn.net/v2/jpg/03/53/11/00/1000_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg')
+    @Default('https://i.ibb.co/d6SZN17/20220320-214742.jpg')
         String profileImage,
     @Default('') String description,
     @Default('') String token,
@@ -33,8 +33,7 @@ UserProfile genRandomUser({bool isExpert = true}) {
   if (isExpert) {
     return const UserProfile(
       nickName: '심규진',
-      profileImage:
-          'https://lh3.googleusercontent.com/TpNzjXhScTNiLV21fzH-lsNYczV5u0fy6bg-mhjZBEf799ftEi3s5kfjJL1O7ggQAq75zduWKMyh7oQp9BAHbYOiss66x_uzl8TI6bmKT2tfettgWjBPh0v9vDoT14SAjzb2DS8FL-WpvtjK8UTkmibcCDR67Kg12xOfQQhY5RvBKjfh3TryXcq6m-TyNKEDPOAXlQDgGyic7eWVVDQDU96QQ_cBKOPkunrxAAX8X53IcAjYXzqnDtvmH4fgaawbdTjUTDGjrOsrhqOVNm44oka6ANXuT1w1KxzVZsSaOLwHXZOlRiyvmCGEvbGYHBa_B0kk16PBmi9LXXQC3QFmYCBD2XCjY8CAYlxO1Nih8j5L8U70_oiLUFL-sEuFfqYIGK0m_5eeExpnl77Z4MHqUe4nuZQqQ-PClHgLYIFfLjeoHzJZxFUvLX_MkrROXRkw1kVttea22uIFIxs8PX6e1IlltcBnLkNFhVuolPaXdeNwm7SbpV_GZQjhJJaSQ6JAYrA6jZfFLZUbmBlw5ZQYgu-kf8gGX_X8cbt9HbkN1FXQ4YxJj2aZLPZYkN_rkCNhi_h9mOtTBM-TKF2deRmzJszbkGLSQIx4rCRje8PBVC_Ws1PvsYQqRu_PWb8x-O1O-JaiC2U4j5ZFAvRsFg7TH-pStiuMV9gIka3Bx4tCRXr1FZsDUg1POByz0b1L2ydtCK85ARmIHYzIgEFroO1ETIfqI3ioA1YHHOUNt2xr694yB7ShnIUp_7GFEhr_ebs=w1003-h1336-no?authuser=0',
+      profileImage: 'https://i.ibb.co/d6SZN17/20220320-214742.jpg',
       uniqueTag: 1234,
       height: 160,
       weight: 50,
@@ -45,8 +44,7 @@ UserProfile genRandomUser({bool isExpert = true}) {
   }
   return UserProfile(
     nickName: '심규진',
-    profileImage:
-        'https://lh3.googleusercontent.com/TpNzjXhScTNiLV21fzH-lsNYczV5u0fy6bg-mhjZBEf799ftEi3s5kfjJL1O7ggQAq75zduWKMyh7oQp9BAHbYOiss66x_uzl8TI6bmKT2tfettgWjBPh0v9vDoT14SAjzb2DS8FL-WpvtjK8UTkmibcCDR67Kg12xOfQQhY5RvBKjfh3TryXcq6m-TyNKEDPOAXlQDgGyic7eWVVDQDU96QQ_cBKOPkunrxAAX8X53IcAjYXzqnDtvmH4fgaawbdTjUTDGjrOsrhqOVNm44oka6ANXuT1w1KxzVZsSaOLwHXZOlRiyvmCGEvbGYHBa_B0kk16PBmi9LXXQC3QFmYCBD2XCjY8CAYlxO1Nih8j5L8U70_oiLUFL-sEuFfqYIGK0m_5eeExpnl77Z4MHqUe4nuZQqQ-PClHgLYIFfLjeoHzJZxFUvLX_MkrROXRkw1kVttea22uIFIxs8PX6e1IlltcBnLkNFhVuolPaXdeNwm7SbpV_GZQjhJJaSQ6JAYrA6jZfFLZUbmBlw5ZQYgu-kf8gGX_X8cbt9HbkN1FXQ4YxJj2aZLPZYkN_rkCNhi_h9mOtTBM-TKF2deRmzJszbkGLSQIx4rCRje8PBVC_Ws1PvsYQqRu_PWb8x-O1O-JaiC2U4j5ZFAvRsFg7TH-pStiuMV9gIka3Bx4tCRXr1FZsDUg1POByz0b1L2ydtCK85ARmIHYzIgEFroO1ETIfqI3ioA1YHHOUNt2xr694yB7ShnIUp_7GFEhr_ebs=w1003-h1336-no?authuser=0',
+    profileImage: 'https://i.ibb.co/d6SZN17/20220320-214742.jpg',
     uniqueTag: 1234,
     height: 160,
     weight: 50,
